1b3bc461c9b1ca2f0dfcfb11337d7da0d5beffc1
AndyRama <andyramaroson@gmail.com>
2021-05-08 23:00:59 +0200

    add migration

---
+++ db/migrate/20210508205916_create_properties.rb
@@ -0,0 +1,19 @@
+class CreateProperties < ActiveRecord::Migration[6.1]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :address
+      t.string :photo
+      t.string :status
+      t.integer :price
+      t.integer :rooms
+      t.integer :bathrooms
+      t.integer :parking_spaces
+      t.boolean :for_sale
+      t.datetime :available_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_204025) do
+ActiveRecord::Schema.define(version: 2021_05_08_205916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "photo"
+    t.string "status"
+    t.integer "price"
+    t.integer "rooms"
+    t.integer "bathrooms"
+    t.integer "parking_spaces"
+    t.boolean "for_sale"
+    t.datetime "available_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "url"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "properties", "users"
 end
