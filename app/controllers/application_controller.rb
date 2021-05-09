ba89e1eaa9db032b58432cfb01d6f837f68784ee
AndyRama <andyramaroson@gmail.com>
2021-05-09 20:48:48 +0200

    add files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # include Pundit
-
-  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
   protected
 
   def configure_permitted_parameters
@@ -18,8 +13,5 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name url image cover_image company number details])
 
-    # def skip_pundit?
-    #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-    # end
   end
 end
