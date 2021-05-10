2b0a914e67716ed560a55b2a3def76c9828253a2
AndyRama <andyramaroson@gmail.com>
2021-05-10 18:01:13 +0200

    clean code routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,14 @@
 Rails.application.routes.draw do
-  root to: 'pages#product'
+  root to: 'public#main'
+
+  resources :posts
+  resources :properties
 
   devise_for :users
 
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
-
-  resources :posts
-  resources :properties
-
-  get "/product" => "pages#product", as: :product
-  
-  get "/home" => "pages#home", as: :home
-
-  get "/compte" => "pages#compte", as: :compte
-  
-  get "/contact" => "pages#contact", as: :contact
-
-  get "/feature" => "pages#feature", as: :feature
-
-  get "/for_sale" => "pages#for_sale", as: :for_sale
-
-  get "/advertise" => "pages#advertise", as: :advertise
-
-  get "/blog" => "posts#latest", as: :blog
 
   get "/users" => "admin#users", as: :users
 
@@ -33,4 +17,15 @@ Rails.application.routes.draw do
   get "/profile/:id" => 'dashboard#profile', as: :profile
 
   post "/agent/message" =>  "properties#email_agent", as: :email_agent
+
+
+  get "/home" => "pages#home", as: :home
+  get "/compte" => "pages#compte", as: :compte  
+  get "/feature" => "pages#feature", as: :feature
+  get "/for_sale" => "pages#for_sale", as: :for_sale
+  get "/advertise" => "pages#advertise", as: :advertise
+  get "/blog" => "posts#latest", as: :blog
+  get "/contact" => "pages#contact", as: :contact
+  get "/product" => "pages#product", as: :product  
+
 end
