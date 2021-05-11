70850c47547a7b7d6ef3a1a9503cc408693f1ccc
AndyRama <andyramaroson@gmail.com>
2021-05-11 14:14:10 +0200

    add product in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,13 @@
 # Product.destroy_all
 
 puts 'Creating category...'
-development = Category.create!(name: 'site MVP')
+development = Category.create!(name: 'Live website MVP')
+service = Category.create!(name: 'customize service')
 
 puts 'Creating products...'
 
-Product.create!(sku: 'MVP LP', name: 'LP PRODUCT MVP', category: development, photo_url: 'https://res.cloudinary.com/dbtnehfrf/image/upload/v1620679520/location_xmaliq.png', price_cents: '19900')
+Product.create!(sku: 'Product Location Property (MVP)', name: 'PRODUCT LOCATION PROPERTY (MVP)', category: development, photo_url: 'https://res.cloudinary.com/dbtnehfrf/image/upload/v1620679520/location_xmaliq.png', price_cents: '19900')
+
+Product.create!(sku: 'Product service customize', name: 'CUSTOMIZE PRODUCT LOCATION PROPERTY', category: service, photo_url: 'https://res.cloudinary.com/dbtnehfrf/image/upload/v1620733317/1613650900263_dqbtuz.jpg', price_cents: '15000')
 
 puts 'Finished!'
